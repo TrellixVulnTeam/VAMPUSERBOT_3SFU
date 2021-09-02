ae3e721c6bc87c65ca264382e670dc10c4c1a8ec
MICKEY♡ <88245294+D15H4NT0P@users.noreply.github.com>
2021-09-02 14:52:42 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -97,8 +97,7 @@ async def on_afk(event):
             f"I'm currently AFK!** \n\n**⏰ AFK Since :**  `{total_afk_time}`\n"
             + f"\n**💬 Reason :"" {reason}`"
   if reason
-            else f"**I'm currently AFK!** \n\n**⏰ AFK Since :**  `{total_afk_time}`\n"+ f"\n**💬 Reason :""{reason}`"\nI'll be back when I feel to come🚶"
-        )
+            else f"**I'm currently AFK!** \n\n**⏰ AFK Since :**  `{total_afk_time}`\n"+ f"\n**💬 Reason : {reason}`\nI'll be back when I feel to come🚶 ")
         msg = await event.reply(message_to_reply, file=VAMPpic)
         await asyncio.sleep(2)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
